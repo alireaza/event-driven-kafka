561314df4d0dc2b4a08e27c406f13b5be980d3a8
AliReaza <alireazamagh@gmail.com>
2022-01-11 05:36:10 +0330

    Update composer.json Cleanup code

--- tests/Unit/ListenerProviderTest.php
+++ tests/Unit/ListenerProviderTest.php
@@ -6,6 +6,11 @@ namespace AliReaza\Tests\EventDriven\Kafka\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class ListenerProviderTest
+ *
+ * @package AliReaza\Tests\EventDriven\Kafka\Unit
+ */
 class ListenerProviderTest extends TestCase
 {
     public function test_Coming_Soon()
